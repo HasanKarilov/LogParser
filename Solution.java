06426861b01fda464e753a51ab2e5b67a0cfb01a
HasanKarilov <hkarilov@gmail.com>
2017-04-23 15:14:52 +0600

    LogParser IPQuery implements 01

--- Solution.java
+++ Solution.java
@@ -1,11 +1,11 @@
-//package com.javarush.task.task39.task3913;
-//
-//import java.nio.file.Paths;
-//import java.util.Date;
-//
-//public class Solution {
-//    public static void main(String[] args) {
-//        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
-//        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
-//    }
-//}
+package com.javarush.task.task39.task3913;
+
+import java.nio.file.Paths;
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+        LogParser logParser = new LogParser(Paths.get("/media/hanaria/Local Disk2/Logger/task39.log"));
+        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+    }
+}
